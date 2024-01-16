7a2b64dbb25bbd4206c444f7ee321ca6187b3ff7
Chris Rhodin <cprhodin@gmail.com>
2024-01-16 01:54:44 -0800

    Move TM1638 strobe pin to INTx pin.

--- project.h
+++ project.h
@@ -59,7 +59,7 @@
 #define SPEAKER_OUT PINMAP_D4
 
 /* TM1638 pins */
-#define TM1638_STB PINMAP_D10
+#define TM1638_STB PINMAP_D2
 
 #define TM1638_STB_HIGH()   pinmap_set(TM1638_STB)
 #define TM1638_STB_LOW()    pinmap_clear(TM1638_STB)
